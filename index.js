156ee2ed1a06d24225b5716f36214a498de91596
stockmopar <git@stockmopar.com>
2014-09-11 20:29:24 -0500

    ...

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ cbtcp.prototype.load = function(host) {
 			}
 			*/
 		});
-		//setTimeout(fetchState,60000);
+		setTimeout(fetchState,20000);
 	};
-	setTimeout(fetchState,20000);
+	setTimeout(fetchState,10000);
 };
 
 /**
